f60d140dc8e08c9e004edff614861c604d15d4e5
Duque-Paola <paosaldan@gmail.com>
2026-01-09 01:18:56 -0600

    Se añade la tarea del laboratorio de modulos y exports.

--- task3.js
+++ task3.js
@@ -1,3 +1,16 @@
 export function ageCalculator(ano,mes,dia) {
+    const fechaAlDia = new Date();
+    const fechaNacimiento = new Date(ano,mes - 1,dia);
 
+    //Este dato que  se mueve, fecha de nacimiento.
+    let edad = fechaAlDia.getFullYear() - fechaNacimiento.getFullYear();
+
+    const diferenciaMeses = fechaAlDia.getMonth() - fechaNacimiento.getMonth();
+    const diferenciaDias = fechaAlDia.getDate() - fechaNacimiento.getDate();
+
+    if (diferenciaMeses < 0 || (diferenciaMeses === 0 && diferenciaDias < 0)) {
+        edad--;
+    }
+
+    return `Tu edad es de ${edad} años.`
 }
